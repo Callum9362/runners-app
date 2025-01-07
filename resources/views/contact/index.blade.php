b0056cabb592bd2ba9da74febc952fa06cc6fd7b
Callum Gill <callum.gill.1993.cg@gmail.com>
2025-01-07 08:14:07 -0800

    feat: :zap: New contact page for fun setting up some pest tests too.

---
+++ resources/views/contact/index.blade.php
@@ -0,0 +1,3 @@
+<div>
+  <p class="text-2xl font-semibold text-gray-800">Contact Us</p>
+</div>
---
+++ tests/Feature/Contact/ContactTest.php
@@ -0,0 +1,10 @@
+<?php
+
+uses()->group('contact');
+
+test('contact page is displayed', function (): void {
+    $response = $this
+        ->get('/contacts');
+
+    $response->assertOk();
+});
